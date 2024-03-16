ebaf06ac2c9732b3afe15e65e577e8ae1959b810
Md. Abdullah Al Fahim <115242534+abdullahfahim2@users.noreply.github.com>
2024-03-16 10:32:27 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,80 +1,122 @@
-#CRUD Operation With Python & SQL
-#edit the Comments for the Operation
 import mysql.connector
 
-con = mysql.connector.connect(host = "localhost" , user = "testuser" , passwd = "test12345" , database = "CompanyList")
+host = input("Enter Your Database Host Name: ")
+user = input("Enter Your Database Username: ")
+passwd = input("Enter Your Database Username: ")
 
+con = mysql.connector.connect(host = host , user = user , passwd = passwd)
 
-"""#Checking Database Connection
-print(con)
-if con:
-    print("Connection Succesfull")
-
-else:
-    print("Connection Unsuccesfull")"""
+#Checking Database Connection
+def check():  
+    print(con)
+    if con:
+        print("Connection Succesfull")
+    else:
+        print("Connection Unsuccesfull")
 
 #creating Cursor in Database
 mycursor = con.cursor()
 
-"""#Creating Database
-mycursor.execute("create database CompanyList;")
-print("Database Created!")"""
+#Creating Database
+def createdb():
+    mycursor.execute("create database CompanyList;")
+    print("Database Created!")
 
-"""#Show Databases
-mycursor.execute("Show Databases;")
+#Show Databases
+def showdb():
+    mycursor.execute("Show Databases;")
+    print("All Databases in Your Host :")
+    for i in mycursor:
+        print(i)
 
-print("All Databases in Your Host :")
+#adding database to con
+datab = input("Enter Database Name: ")
+con = mysql.connector.connect(host = host , user = user , passwd = passwd , database = datab)
+mycursor = con.cursor()
 
-for i in mycursor:
-    print(i)"""
+#create table in your Database
+def create_table():
+    mycursor.execute("create table employee(no INT , Name VARCHAR(55) , Designation VARCHAR(55) , Salary INT);")
+    print("Table Created In Your Selected Database!")
 
-"""#create table in your Database
-#Firstly Connect your database with con Variable by adding database attribute and your database as value!
+#Insert Data in Table
+def insert_data():
+    #Single Data Entry
+    #mycursor.execute("insert into employee(no , Name , Designation , Salary) values (101 , 'karim' , 'manager' , 20000)")
 
-mycursor.execute("create table employee(no INT , Name VARCHAR(55) , Designation VARCHAR(55) , Salary INT);")
-print("Table Created In Your Selected Database!")"""
+    #multiple Data Entry
+    insertdata = ("insert into employee(no , Name , Designation , Salary) values (%s , %s , %s , %s)")
+    value = [
+        (102 , 'Shamim' , 'Marketing Head' , 18000),
+        (103 , 'Tanbir' , 'Graphics Designer' , 18000),
+        (104 , 'Jilane' , 'Network administrator' , 18000),
+        (104 , 'Hasan' , 'Clerk' , 12000)
+    ]
 
-"""#Insert Data in Table
-#Single Data Entry
-#mycursor.execute("insert into employee(no , Name , Designation , Salary) values (101 , 'karim' , 'manager' , 20000)")
+    mycursor.executemany(insertdata , value)
 
-#multiple Data Entry
-insertdata = ("insert into employee(no , Name , Designation , Salary) values (%s , %s , %s , %s)")
-value = [
-    (102 , 'Shamim' , 'Marketing Head' , 18000),
-    (103 , 'Tanbir' , 'Graphics Designer' , 18000),
-    (104 , 'Jilane' , 'Network administrator' , 18000),
-    (104 , 'Hasan' , 'Clerk' , 12000)
-]
+    con.commit()
+    print("Data Inserted To Your Selected Database Table!")
 
-mycursor.executemany(insertdata , value)
+#read data from database table
+def read_data():
+    mycursor.execute("select * from employee;")
+    result = mycursor.fetchall()
 
-con.commit()
-print("Data Inserted To Your Selected Database Table!")"""
+    print("Data in Your Table : ")
 
-"""#read data from database table
-mycursor.execute("select * from employee;")
-result = mycursor.fetchall()
+    for i in result:
+        print(i)
 
-print("Data in Your Table : ")
+#update data in table
+def update_data():
+    mycursor.execute("update employee set Salary = 15000 where no = 101;")
+    con.commit()
+    print("Your Data Updated!")
 
-for i in result:
-    print(i)"""
+#Delete Data in Table
+def delete_data():
+    mycursor.execute("delete from employee where no = 105;")
+    con.commit()
+    print("Your Selected Data Is Deleted!")
 
-"""#update data in table
+#Search from table
+def search_data():
+    mycursor.execute("select * from employee where no =104;")
+    s = mycursor.fetchone()
+    print(s)
 
-mycursor.execute("update employee set Salary = 15000 where no = 101;")
-con.commit()
-print("Your Data Updated!")"""
 
-"""#Delete Data in Table
 
-mycursor.execute("delete from employee where no = 105;")
-con.commit()
-print("Your Selected Data Is Deleted!")"""
+print("Select Your Preferd Operations:\n1.Check Your Database Connection\n2.Create Database\n3.Show Database")
+print("4.Create Table\n5.Insert Data in Table\n6.Read Data\n7.Update Data\n8.Delete Data\n9.Search Data\n10.Exit")
 
-"""#Search from table
+while True:
 
-mycursor.execute("select * from employee where no =104;")
-s = mycursor.fetchone()
-print(s)"""
+    ch = int(input("Enter Your Choice: \n"))
+
+    if ch == 1:
+        check()
+    elif ch == 2:
+        createdb()
+    elif ch == 3:
+        showdb()
+    elif ch == 4:
+        create_table()
+    elif ch == 5:
+        insert_data()
+    elif ch == 6:
+        read_data()
+    elif ch == 7:
+        update_data()
+    elif ch == 8:
+        delete_data()
+    elif ch == 9:
+        search_data()
+    elif ch == 10:
+        print("Thanks For Using This System!\nFeel Free To Ask Me Anything!")
+        print("Contact Me : aafahim4320@gmail.com")
+        break
+        
+    else:
+        print("You Entered an Invalid Choice!")
